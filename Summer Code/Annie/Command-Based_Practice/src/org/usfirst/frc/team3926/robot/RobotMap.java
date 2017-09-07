785974b483ef49f9c91ed639eed6619aa7f2f1d2
APortoghese <aportoghese20@moundsparkacademy.org>
2017-09-07 16:02:11 -0500

    practice

---
+++ Summer Code/Annie/Command-Based_Practice/src/org/usfirst/frc/team3926/robot/RobotMap.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team3926.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+    public static int FRONT_RIGHT_MOTOR_ID = 2; //inputs for Jill
+    public static int FRONT_LEFT_MOTOR_ID  = 14;
+    public static int BACK_RIGHT_MOTOR_ID  = 3;
+    public static int BACK_LEFT_MOTOR_ID   = 15;
+
+    public static int SHOOTER_MOTOR_ID     = 10; //add correct input
+    public static int CLIMBING_MOTOR_ID    = 11; //add correct input
+
+    public static int XBOX_CONTROLLER_ID   = 0;
+    public static int RIGHT_JOYSTICK_ID    = 1;
+    public static int LEFT_JOYSTICK_ID     = 2;
+    
+    public static int SHOOTER_BUTTON_ID    = 12;
+
+}
---
+++ Summer Code/Annie/Command-Based_Practice/src/org/usfirst/frc/team3926/robot/commands/DriveCommand.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team3926.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team3926.robot.OI;
+import org.usfirst.frc.team3926.robot.Robot;
+
+/**
+ *
+ */
+public class DriveCommand extends Command {
+	public DriveCommand() {
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.DrivingSubsystem);
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		
+		Robot.DrivingSubsystem.TankDrive(OI.lJoystick.getY(), OI.rJoystick.getY());
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		
+		Robot.DrivingSubsystem.TankDrive(0, 0);
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+}
