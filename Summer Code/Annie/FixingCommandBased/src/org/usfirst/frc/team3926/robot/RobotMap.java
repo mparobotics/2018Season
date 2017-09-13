85faf7b9687aa8b25643d35f564a5103678d9d4d
APortoghese <aportoghese20@moundsparkacademy.org>
2017-09-13 16:54:44 -0500

    fixing all the things

--- Summer Code/Annie/FixingCommandBased/src/org/usfirst/frc/team3926/robot/RobotMap.java
+++ Summer Code/Annie/FixingCommandBased/src/org/usfirst/frc/team3926/robot/RobotMap.java
@@ -8,10 +8,10 @@ package org.usfirst.frc.team3926.robot;
  */
 public class RobotMap {
 
-    public final static int FRONT_RIGHT_MOTOR_ID      = 2; //inputs for Jill (might need to change)
-    public final static int FRONT_LEFT_MOTOR_ID       = 14;
-    public final static int BACK_RIGHT_MOTOR_ID       = 3;
-    public final static int BACK_LEFT_MOTOR_ID        = 15;
+    public final static int FRONT_RIGHT_MOTOR_ID      = null; //inputs for Jill (might need to change)
+    public final static int FRONT_LEFT_MOTOR_ID       = 12;
+    public final static int BACK_RIGHT_MOTOR_ID       = null;
+    public final static int BACK_LEFT_MOTOR_ID        = 15; 
 
 
     public final static boolean XBOX_DRIVE_CONTROLLER = false;
---
+++ Summer Code/Annie/FixingCommandBased/src/org/usfirst/frc/team3926/robot/subsystems/DrivingSubsystem.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team3926.robot.subsystems;
+
+import org.usfirst.frc.team3926.robot.RobotMap;
+import org.usfirst.frc.team3926.robot.commands.DriveCommand;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DrivingSubsystem extends Subsystem {
+
+	CANTalon FRmotor = new CANTalon(RobotMap.FRONT_RIGHT_MOTOR_ID);
+	CANTalon FLmotor = new CANTalon(RobotMap.FRONT_LEFT_MOTOR_ID);
+	CANTalon BRmotor = new CANTalon(RobotMap.BACK_RIGHT_MOTOR_ID);
+	CANTalon BLmotor = new CANTalon(RobotMap.BACK_LEFT_MOTOR_ID);
+	
+	RobotDrive robotDrive = new RobotDrive(FRmotor, FLmotor, BRmotor, BLmotor);
+	
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	public void initDefaultCommand() {
+		
+		new DriveCommand();
+		// Set the default command for a subsystem here.
+		//setDefaultCommand(new MySpecialCommand());
+	}
+	public void TankDrive(double lSpeed, double rSpeed) {
+		
+		robotDrive.tankDrive(lSpeed, rSpeed);
+	}
+}
