68010e37905e5370636a7dd5a47e642aed232989
APortoghese <aportoghese20@moundsparkacademy.org>
2018-02-20 15:30:02 -0600

    maybe competition code

--- TestBench/src/main/java/frc/team3926/robot/SketchyAuto.java
+++ TestBench/src/main/java/frc/team3926/robot/SketchyAuto.java
@@ -34,12 +34,53 @@ public class SketchyAuto extends CommandGroup {
         // a CommandGroup containing them would require both the chassis and the
         // arm.
 
-        //posistion = DriverStation.getInstance().getLocation();
+        posistion = DriverStation.getInstance().getLocation();
         switchPosistion = DriverStation.getInstance().getGameSpecificMessage();
-        posistion = 2;
+        //posistion = 2;
         time = Timer.getFPGATimestamp();
 
         //TODO diffrent things for diffrent posistions
+
+        //sides
+        /*if(posistion == 1 && switchPosistion.startsWith("l")) {
+
+            addSequential(new AutoGoStraightSides()); //score on switch
+
+        } else if (posistion == 1 && switchPosistion.startsWith("r")) {
+
+           //dont score on switch
+        }
+        if(posistion == 3 && switchPosistion.startsWith("r")) {
+
+            addSequential(new AutoGoStraightSides()); //score on switch
+
+        } else if (posistion == 3 && switchPosistion.startsWith("l")) {
+
+            //dont score on switch
+        }
+
+        //center
+        if(posistion == 2 && switchPosistion.startsWith("r")) {
+
+            addSequential(new AutoStraightMiddle()); //turn right to score
+
+            addSequential(new AutoTurnRight());
+            addSequential(new AutoStraightMiddle());
+
+            addSequential(new AutoTurnLeft());
+            addSequential(new CenterSlam());
+
+        } else if(posistion == 2 && switchPosistion.startsWith("l")) {
+
+            addSequential(new AutoStraightMiddle());
+
+            addSequential(new AutoTurnLeft());
+            addSequential(new AutoStraightMiddle());
+
+            addSequential(new AutoTurnRight());
+            addSequential(new CenterSlam());
+        }*/
+
 
         if (posistion == 1 || posistion == 3) {
 
